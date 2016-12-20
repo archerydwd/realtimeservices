8605c4af961499a6185009c30fb38de2eac9bb7b
Darren Daly <darren.daly@teamaol.com>
2016-12-20 18:02:27 +0000

    small refactor

--- trains.py
+++ trains.py
@@ -2,8 +2,6 @@ from __future__ import print_function
 import requests
 import xmltodict
 
-# --------------- Helpers that build all of the responses ----------------------
-
 stations = {
     "dublin heuston": "http://api.irishrail.ie/realtime/realtime.asmx/getStationDataByCodeXML_WithNumMins?StationCode=HSTON&NumMins=60",
     "dublin connolly": "http://api.irishrail.ie/realtime/realtime.asmx/getStationDataByCodeXML_WithNumMins?StationCode=CNLLY&NumMins=60",
@@ -122,6 +120,8 @@ stations = {
     "dundalk": "http://api.irishrail.ie/realtime/realtime.asmx/getStationDataByCodeXML_WithNumMins?StationCode=DDALK&NumMins=90"
 }
 
+# --------------- Helpers that build all of the responses ----------------------
+
 def build_speechlet_response(title, output, reprompt_text, should_end_session):
     return {
         'outputSpeech': {
@@ -218,30 +218,21 @@ def set_train_station_session(intent, session):
 
 def on_session_started(session_started_request, session):
     """ Called when the session starts """
-
     print("on_session_started requestId=" + session_started_request['requestId']
           + ", sessionId=" + session['sessionId'])
 
 def on_launch(launch_request, session):
-    """ Called when the user launches the skill without specifying what they
-    want
-    """
-
+    """ Called when the user launches the skill without specifying what they want """
     print("on_launch requestId=" + launch_request['requestId'] +
           ", sessionId=" + session['sessionId'])
-    # Dispatch to your skill's launch
     return get_welcome_response()
 
 def on_intent(intent_request, session):
     """ Called when the user specifies an intent for this skill """
-
     print("on_intent requestId=" + intent_request['requestId'] +
           ", sessionId=" + session['sessionId'])
-
     intent = intent_request['intent']
     intent_name = intent_request['intent']['name']
-
-    # Dispatch to your skill's intent handlers
     if intent_name == "GetTrain":
         return set_train_station_session(intent, session)
     else:
@@ -249,12 +240,9 @@ def on_intent(intent_request, session):
 
 def on_session_ended(session_ended_request, session):
     """ Called when the user ends the session.
-
-    Is not called when the skill returns should_end_session=true
-    """
+    Is not called when the skill returns should_end_session=true """
     print("on_session_ended requestId=" + session_ended_request['requestId'] +
           ", sessionId=" + session['sessionId'])
-    # add cleanup logic here
 
 # --------------- Main handler ------------------
 
@@ -265,14 +253,9 @@ def lambda_handler(event, context):
     print("event.session.application.applicationId=" +
           event['session']['application']['applicationId'])
 
-    """
-    Uncomment this if statement and populate with your skill's application ID to
-    prevent someone else from configuring a skill that sends requests to this
-    function.
-    """
-    # if (event['session']['application']['applicationId'] !=
-    #         "amzn1.echo-sdk-ams.app.[unique-value-here]"):
-    #     raise ValueError("Invalid Application ID")
+    if (event['session']['application']['applicationId'] !=
+            "amzn1.ask.skill.69c6262f-8957-45aa-b07b-4ba55e1a846d"):
+        raise ValueError("Invalid Application ID")
 
     if event['session']['new']:
         on_session_started({'requestId': event['request']['requestId']},
